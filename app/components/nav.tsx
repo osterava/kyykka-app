e037f4942ee6cd37df688230bdb0b229d277d47d
osterava <olli.s.terava@student.jyu.fi>
2025-12-15 16:54:10 +0200

    mobiili-css

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,15 +1,42 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 
 export default function Nav() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="nav">
-      <ul className="nav-list">
-        <li><Link href="/">Jyväskylän mestaruuskyykkä</Link></li>
-        <li><Link href="/aikataulu">Aikataulu</Link></li>
-        <li><Link href="/lohkojako">Lohkojako</Link></li>
-        <li><Link href="/tulokset">Tulokset</Link></li>
-        <li><Link href="/saannot">Säännöt</Link></li>
-      </ul>
+      <div className="nav-header">
+        <Link href="/" className="brand">
+          Jyväskylän mestaruuskyykkä 2026
+        </Link>
+
+        <button
+          className="burger"
+          onClick={() => setOpen(!open)}
+          aria-label="Avaa valikko"
+        >
+          ☰
+        </button>
+
+        <span
+          className="desktop-menu"
+          onClick={() => setOpen(!open)}
+        >
+          Valikko
+        </span>
+      </div>
+
+      <ul className={`nav-list ${open ? "open" : ""}`}>
+          <li>Valikko</li>
+          <span className="line"></span>
+          <li><Link href="/aikataulu">Aikataulu</Link></li>
+          <li><Link href="/lohkojako">Lohkojako</Link></li>
+          <li><Link href="/tulokset">Tulokset</Link></li>
+          <li><Link href="/saannot">Säännöt</Link></li>
+        </ul>
     </nav>
   );
 }
